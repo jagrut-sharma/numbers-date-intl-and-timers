963cea9c9ce34005f107775718b9ebdaff3b9e99
Jagrut <jagrut.sharma1@gmail.com>
2022-12-15 20:26:30 +0530

    upto conversion of numbers

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,41 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+console.log(23 === 23.0);
+console.log(0.1 + 0.2);
+console.log(0.3 === 0.1 + 0.2);
+
+// Conversion:
+console.log(Number(23));
+console.log('23');
+console.log(+'23');
+console.log(+'23X');
+
+//Parsing:
+console.log(Number.parseInt('30px')); // 23 --> automatically extracts 23 from string
+console.log(Number.parseInt('px35sda')); // NaN --> Number should be first
+console.log(Number.parseInt('   50rem    ')); // NaN --> Even if spaces are present, they will be ignored
+
+console.log(Number.parseFloat('2.5rem')); // 2.5
+console.log(Number.parseFloat('  2.5rem      ')); // 2.5
+console.log(Number.parseInt('  2.5rem      ')); // 2 --> as it is int
+
+// Check if value is NaN
+
+console.log(Number.isNaN(45)); // false
+console.log(Number.isNaN('45')); // false
+console.log(Number.isNaN(+'50XE')); // true --> because +'50XE' is NaN, only parseInt automatically took value
+console.log(Number.isNaN(23 / 0)); // false --> its infinity
+
+// To check if value is number:
+console.log(Number.isFinite(20)); // true
+console.log(Number.isFinite('20')); // false
+console.log(Number.isFinite(+'20X')); // false
+console.log(Number.isFinite('20/0')); // false
+
+// TO check if its interger
+console.log(Number.isInteger(75)); // true
+console.log(Number.isInteger('75')); // false
+console.log(Number.isInteger(75.0)); // true --> as it is still a integer
+console.log(Number.isInteger(75 / 0)); // false
